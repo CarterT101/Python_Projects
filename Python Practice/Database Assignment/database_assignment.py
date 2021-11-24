2b7a18f4d7337868ee39b9a136815541de533b42
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2021-11-24 14:07:38 -0800

    added comments

--- Python Practice/Database Assignment/database_assignment.py
+++ Python Practice/Database Assignment/database_assignment.py
@@ -8,29 +8,29 @@ import sqlite3
 #print(help(sqlite3))
 
 
-conn = sqlite3.connect('files.db')
+conn = sqlite3.connect('files.db') #connecting variable to database
 
 
 with conn:
-    cur = conn.cursor()
+    cur = conn.cursor() #can edit table
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
         col_file TEXT \
-        )")
-    conn.commit()
-conn.close()
+        )") #executes sql statements
+    conn.commit() #commits it to database
+conn.close() #closes database
 
 
-fileList = ('information.docx','Hello.txt','myImage.png','myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+fileList = ('information.docx','Hello.txt','myImage.png','myMovie.mpg','World.txt','data.pdf','myPhoto.jpg') #list of files
 
 
 conn = sqlite3.connect('files.db')
 
 with conn:
     cur = conn.cursor()
-    for file in fileList:
-        if file.endswith('.txt'):
-            cur.execute("INSERT INTO tbl_files(col_file) VALUES (?)", (file,))
+    for file in fileList: #goes through created list 
+        if file.endswith('.txt'): #finds files that end with .txt
+            cur.execute("INSERT INTO tbl_files(col_file) VALUES (?)", (file,)) #inserts each into table through iterations
             print(file)
     conn.commit()
 conn.close()
@@ -39,7 +39,7 @@ conn = sqlite3.connect('files.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("SELECT col_file FROM tbl_files")
+    cur.execute("SELECT col_file FROM tbl_files") #selecting column from table
     varFiles = cur.fetchall()
     for item in varFiles:
         listitems = "First Document: {}\nSecond Document: {}".format(item[0],item[1])
