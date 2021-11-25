40b182d6eaf2747b60696569cc2065d6e96affbf
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2021-11-24 16:38:36 -0800

    Update classInheritance.py

--- Python Practice/classInheritance.py
+++ Python Practice/classInheritance.py
@@ -3,16 +3,16 @@
 
 class newVehicle:
     brand = ""
-    model = ""
+    model = "" #extreme basic attributes of a vehicle
     year = ""
 
 
 
-class Car(newVehicle):
-    wheels = ""
+class Car(newVehicle): #creating new class connected to parent class newVehicle
+    wheels = "" 
     doors = ""
 
-class Boat(newVehicle):
+class Boat(newVehicle): #creating new class connected to parent class
     length = ""
     boatCategory = ""
     
